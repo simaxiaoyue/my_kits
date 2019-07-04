88889c6d84403c54a10c6c4d4c97d8d03208c697
carrot cat <carrot cat@gmail.com>
2019-07-04 13:53:27 +0800

    工具类

--- kits.js
+++ kits.js
@@ -4,3 +4,20 @@ var kits = {};
 kits.randonInt = function (n, m) {
   return Math.floor(Math.random() * (m - n + 1) + n);
 }
+
+// 封装一个获取当前时间的方法
+kits.formateDate = function(){
+  let date = new Date();
+  let y = date.getFullYear();
+  let M = date.getMonth() + 1;
+  let d = date.getDate();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  M = M < 10 ? '0' + M : M;
+  d = d < 10 ? '0' + d : d;
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+  return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s;
+}
