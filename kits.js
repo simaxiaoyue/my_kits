d3f68bf0f9f273cbdecd2aa20f1dd87bc9d605b1
carrot cat <carrot cat@gmail.com>
2019-07-05 23:02:32 +0800

    工具类

--- kits.js
+++ kits.js
@@ -1,7 +1,7 @@
 //封装常用的js代码
 var kits = {};
 // 封装一个获取指定区间的随机整数的方法
-kits.randonInt = function (n, m) {
+kits.randomInt = function (n, m) {
   return Math.floor(Math.random() * (m - n + 1) + n);
 }
 
